6f43d1bfde13c2e1eae5e1b6b57365ef1b2a2fd7
Marco Leong <marcogood411@gmail.com>
2009-09-10 16:23:49 +0800

    Start developing collabquiz, version 2009091000

---
+++ version.php
@@ -0,0 +1,7 @@
+<?php
+
+$module->version  = 2009091000;   // The (date) version of this module
+$module->requires = 2007101509;   // Requires this Moodle version
+$module->cron     = 0;            // How often should cron check this module (seconds)?
+
+?>
